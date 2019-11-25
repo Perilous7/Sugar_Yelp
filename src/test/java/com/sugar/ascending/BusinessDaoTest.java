bf896dd30cbb85cebd0216d30108021fb44740f6
Ruichen Zhou <ruichen.zhou82@gmail.com>
2019-11-25 14:37:29 -0500

    unitTestForBusiness

--- src/test/java/com/sugar/ascending/BusinessDaoTest.java
+++ src/test/java/com/sugar/ascending/BusinessDaoTest.java
@@ -31,19 +31,19 @@ public class BusinessDaoTest {
     public void insertBusinessTest(){
         List<Business> businesses = businessDao.getBusinesses();
         int NumOfBusinessBeforeInsert = businesses.size();
-
-        Business business = new Business();
-        business.setName("Peter Chang");
-        business.setAddress("2503-E N Harrison St, Arlington, VA 22207");
-        business.setCategory("restaurant");
-        business.setHours("Monday 11:30AM–3PM, 5–9:45PM; Tuesday 11:30AM–3PM, 5–9:45PM; Wednesday 11:30AM–3PM, 5–9:45PM; Thursday 11:30AM–3PM, 5–9:45PM;"+
-                "Friday 11:30AM–9:45PM; Saturday 11:30AM–9:45PM; Sunday 11:30AM–8:45PM");
-        businessDao.insertBusiness(business);
+        //create test business
+        Business testb = new Business();
+        testb.setName("test");
+        testb.setAddress("some rd");
+        testb.setCategory("food");
+        testb.setHours("Five days a week");
+        businessDao.insertBusiness(testb);
 
         List<Business> businessesAfterInsert = businessDao.getBusinesses();
         int expectedNumOfBusinessAfterInsert = businessesAfterInsert.size();
         Assert.assertEquals(expectedNumOfBusinessAfterInsert-1,NumOfBusinessBeforeInsert);
-
+        //delete test business
+        businessDao.deleteBusiness("name = 'test'");
 
     }
 
@@ -52,6 +52,7 @@ public class BusinessDaoTest {
         List<Business> businesses = businessDao.getBusinesses();
         int NumOfBusinessBeforeDelete = businesses.size();
 
+        //delete one record as test
         String condition = "name = 'Peter Chang'";
         businessDao.deleteBusiness(condition);
 
@@ -59,6 +60,7 @@ public class BusinessDaoTest {
         int expectedNumOfBusinessAfterDelete = businessesAfterDelete.size();
         Assert.assertEquals(expectedNumOfBusinessAfterDelete,NumOfBusinessBeforeDelete-1);
 
+        //add back
         Business b = new Business();
         b.setName("Peter Chang");
         b.setAddress("2503-E N Harrison St, Arlington, VA 22207");
@@ -71,16 +73,25 @@ public class BusinessDaoTest {
 
     @Test
     public void updateBusinessTest(){
+        //create test data
+        Business testb = new Business();
+        testb.setName("test");
+        testb.setAddress("some rd");
+        testb.setCategory("food");
+        testb.setHours("Five days a week");
+        businessDao.insertBusiness(testb);
         List<Business> businesses = businessDao.getBusinesses();
         int NumOfBusinessBeforeUpdate = businesses.size();
 
         String setStatement = "category  = 'Good restaurant'";
-        String condition = "name = 'Peter Chang'";
+        String condition = "name = 'test'";
         businessDao.updateBusiness(setStatement, condition);
 
         List<Business> businessesAfterUpdate = businessDao.getBusinesses();
         int expectedNumOfBusinessAfterUpdate = businessesAfterUpdate.size();
         Assert.assertEquals(expectedNumOfBusinessAfterUpdate,NumOfBusinessBeforeUpdate);
+        //delete test data
+        businessDao.deleteBusiness(condition);
     }
 
 }
